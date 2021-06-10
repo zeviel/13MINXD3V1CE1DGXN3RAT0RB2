1101a9f64cef1a41fb99fb9af4b342794820b44f
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-06-10 09:42:34 +0600

    Update AminoDeviceIdGeneratorBo.py

--- AminoDeviceIdGeneratorBo.py
+++ AminoDeviceIdGeneratorBo.py
@@ -3,22 +3,17 @@ import string
 import random
 import json
 import time
+import pyfiglet
 from hashlib import sha1
 from progress.bar import IncrementalBar
 from colorama import init, Fore, Back, Style
 init()
-print(Back.BLACK)
 print(Fore.GREEN)
 print(Style.NORMAL)
 print("""Script by Lil Zevi
-Github : https://github.com/LilZevi
-▄▀▄ █▄░▄█ ▀ █▄░█ ▄▀▄ █▀▄ █▀▀ ▐▌░▐▌ ▀ ▄▀ █▀▀ ▀ █▀▄
-█▀█ █░█░█ █ █░▀█ █░█ █░█ █▀▀ ░▀▄▀░ █ █░ █▀▀ █ █░█
-▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░ ▀▀░ ▀▀▀ ░░▀░░ ▀ ░▀ ▀▀▀ ▀ ▀▀░
-▄▀▀░ █▀▀ █▄░█ █▀▀ █▀▀▄ ▄▀▄ ▀█▀ ▄▀▄ █▀▀▄ █▀▄ ▄▀▄
-█░▀▌ █▀▀ █░▀█ █▀▀ █▐█▀ █▀█ ░█░ █░█ █▐█▀ █▀█ █░█
-▀▀▀░ ▀▀▀ ▀░░▀ ▀▀▀ ▀░▀▀ ▀░▀ ░▀░ ░▀░ ▀░▀▀ ▀▀░ ░▀░""")
-
+Github : https://github.com/LilZevi""")
+print(pyfiglet.figlet_format("aminodeviceid", font="bell"))
+print(pyfiglet.figlet_format("generatorbo", font="bell"))
 def deviceIdgenerator(st : int = 69):
     ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k = st))
     deviceid = '01' + (MetaSpecial := sha1(ran.encode("utf-8"))).hexdigest() + sha1(bytes.fromhex('01') + MetaSpecial.digest() + base64.b64decode("6a8tf0Meh6T4x7b0XvwEt+Xw6k8=")).hexdigest()
