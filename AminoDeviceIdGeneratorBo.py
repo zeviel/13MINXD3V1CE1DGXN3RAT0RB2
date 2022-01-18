3238e26b08c3cdbff75f8bcf92c18424bdc36f58
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-01-18 13:25:38 +0600

    Update AminoDeviceIdGeneratorBo.py

--- AminoDeviceIdGeneratorBo.py
+++ AminoDeviceIdGeneratorBo.py
@@ -1,36 +1,29 @@
-import base64
-import string
-import random
-import json
-import time
-import pyfiglet
+from os import urandom
 from hashlib import sha1
+from base64 import b64decode
+from pyfiglet import figlet_format
 from progress.bar import IncrementalBar
-from colorama import init, Fore, Back, Style
-init()
-print(Fore.GREEN)
-print(Style.NORMAL)
-print("""Script by Lil Zevi
-Github : https://github.com/LilZevi""")
-print(pyfiglet.figlet_format("aminodeviceid", font="bell"))
-print(pyfiglet.figlet_format("generatorbo", font="bell"))
-def deviceIdgenerator(st : int = 69):
-    ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k = st))
-    deviceid = '01' + (MetaSpecial := sha1(ran.encode("utf-8"))).hexdigest() + sha1(bytes.fromhex('01') + MetaSpecial.digest() + base64.b64decode("6a8tf0Meh6T4x7b0XvwEt+Xw6k8=")).hexdigest()
-    return deviceid
+from colorama import init, Fore, Style; init()
+print(Fore.GREEN + Style.NORMAL)
+print("""Script by deluvsushi
+Github : https://github.com/deluvsushi""")
+print(figlet_format("aminodeviceidgeneratorbo", font="bell"))
 
-def deviceIdgenerationproccess():
+def deviceID_generator(hwid: str):
+    return "01" + (identifier := sha1(hwid.encode("utf-8"))).hexdigest() + sha1(bytes.fromhex("01") + identifier.digest() + base64.b64decode("6a8tf0Meh6T4x7b0XvwEt+Xw6k8=")).hexdigest()
+
+def generation_process(count: int):
 	try:
-		bar = IncrementalBar('Generated', max = generatingnumber)
-		for i in range(generatingnumber):
-			device = deviceIdgenerator()
-			devfile = open('deviceids.txt', "a+")
-			devfile.write(device + '\n')
-			bar.next()
+		progress_bar = IncrementalBar("Generated", max=count)
+		with open ("device_Ids.txt" "a") as device_Ids:
+			for i in range(count):
+				device_Id = deviceID_generator(hwid=urandom(20))
+				device_Ids.write(f"{device_Id}\n")
+				bar.next()
+			print(f"-- Successful generated {count} deviceIds!")
+			bar.finish()
 	except:
 		return
 
-generatingnumber = int(input("How Much DeviceId Generate: "))
-
-deviceIdgenerationproccess()
-print(f"\nGenerated {generatingnumber} deviceids")
+count = int(input("-- How many deviceIDs?::: "))
+generation_process(count=count)
