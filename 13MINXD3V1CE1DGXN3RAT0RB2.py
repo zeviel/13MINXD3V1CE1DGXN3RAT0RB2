4ae30f4e660f1513a482fb3ea89f6c01bc38864d
zeviel <77536370+zeviel@users.noreply.github.com>
2022-08-25 15:16:04 +0800

    Update 13MINXD3V1CE1DGXN3RAT0RB2.py

--- 13MINXD3V1CE1DGXN3RAT0RB2.py
+++ 13MINXD3V1CE1DGXN3RAT0RB2.py
@@ -3,22 +3,20 @@ from hashlib import sha1
 from base64 import b64decode
 from pyfiglet import figlet_format
 from progress.bar import IncrementalBar
-from colorama import init, Fore, Style; init()
-print(Fore.GREEN + Style.NORMAL)
-print("""Script by deluvsushi
-Github : https://github.com/deluvsushi""")
+print("""Script by zeviel
+Github : https://github.com/zeviel""")
 print(figlet_format("13MINXD3V1CE1DGXN3RAT0RB2", font="bell"))
 
-def deviceID_generator(hwid: str):
-    return "01" + (identifier := sha1(hwid.encode("utf-8"))).hexdigest() + sha1(bytes.fromhex("01") + identifier.digest() + base64.b64decode("6a8tf0Meh6T4x7b0XvwEt+Xw6k8=")).hexdigest()
+def generate_device_id(hwid: str):
+    return "01" + (identifier := sha1(hwid.encode("utf-8"))).hexdigest() + sha1(bytes.fromhex("01") + identifier.digest() + b64decode("6a8tf0Meh6T4x7b0XvwEt+Xw6k8=")).hexdigest()
 
 def generation_process(count: int):
 	try:
 		progress_bar = IncrementalBar("Generated", max=count)
-		with open ("device_Ids.txt" "a") as device_Ids:
+		with open ("device_ids.txt" "a") as device_Ids:
 			for i in range(count):
-				device_Id = deviceID_generator(hwid=urandom(20))
-				device_Ids.write(f"{device_Id}\n")
+				device_id = generate_device_id(urandom(20))
+				device_ids.write(f"{device_id}\n")
 				bar.next()
 			print(f"-- Successful generated {count} deviceIds!")
 			bar.finish()
